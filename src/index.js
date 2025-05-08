bc3824316202e539f5e7a3473aa1dfa8c8743773
Daniele Pauli <danielepauli@Danieles-MacBook-Air.local>
2025-05-08 14:42:59 +0700

    Initial commit of Daniele Pauli Coaching website

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Make sure this element exists in your HTML
+const container = document.getElementById('root');
+
+// Check if container exists before creating root
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found. Make sure there is a <div id="root"></div> in your HTML file.');
+}
